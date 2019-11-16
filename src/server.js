310a45e1f97a5ab42553fb64864a2258dcbc29ec
madi-madi <ibrahim.s.m.2010@gmail.com>
2019-11-16 17:25:13 +0200

    route api

--- src/server.js
+++ src/server.js
@@ -1,4 +1,56 @@
 const  express = require('express');
 const app =  express();
 const PORT = 3000;
+
+
+
+// get all contacts 
+app.get('/api/contacts',(request, response)=>{
+    response.status(200).json({
+        message:"All Contact Info :)"
+    })
+});
+
+
+// get spicific id contacts
+app.get('/api/contacts/:id',(request ,response)=>{
+    response.status(200).json({
+        message:"get spicific id contacts",
+         contact_id : request.params.id
+    })
+});
+
+// create new  contacts 
+app.post('/api/contacts/create',(request ,response)=>{
+    response.status(201).json({
+        message:"Create Contacts successfuly",
+        contact_id : request.params.id
+    })
+});
+
+// partially update 
+app.patch('/api/contacts/:id/update',(request ,response)=>{
+    response.status(201).json({
+        message:"Update Contacts Successfuly",
+        contact_id : request.params.id
+    })
+});
+
+// completly update (create new and replace old )
+app.put('/api/contacts/:id/create',(request,response)=>{
+    response.status(201).json({
+        message: "Update all contacts put",
+        contact_id : request.params.id
+    })
+});
+
+// delete contacts
+app.delete('/api/contacts/:id/delete',(request ,response)=>{
+    response.status().json({
+        message:"Delete Contact Successfuly",
+        contact_id : request.params.id
+    })
+});
+
+
 app.listen(PORT,()=> console.log('kkkkkkk'));
